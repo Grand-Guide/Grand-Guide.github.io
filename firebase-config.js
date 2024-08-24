832f417a147b17c401e10ffc29e17ec3c4f0764c
Linwaru yt <linwaru@outlook.com>
2024-08-24 21:10:41 +0000

    firebase update moduler

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIvahfuFgjSxDDOaN_c9X1MlUJ1qYb2xA",
+  authDomain: "grandguide.firebaseapp.com",
+  projectId: "grandguide",
+  storageBucket: "grandguide.appspot.com",
+  messagingSenderId: "498781308632",
+  appId: "1:498781308632:web:9d54b99d3c0e98dae939ad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
